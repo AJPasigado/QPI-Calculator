c44f1661a74b27d60ed7df5daac613090f0685c2
AJ Pasigado <ajpasigado@outlook.com>
2018-10-30 02:22:05 +0800

    QPI Calculator Version 2.4; RecyclerView for Grades; Swipe to delete, Delete All notif; Gesture Listeners; Removes Calculator Class

--- app/src/main/java/com/ajpasigado/qpicalculator/MainActivity.java
+++ app/src/main/java/com/ajpasigado/qpicalculator/MainActivity.java
@@ -1,25 +1,31 @@
 package com.ajpasigado.qpicalculator;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.TypedValue;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Space;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.animation.ValueAnimator;
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    public Integer count = 100;
-    CalculatorMain cm = new CalculatorMain();
+public class MainActivity extends AppCompatActivity implements GradesRecyclerItemTouchHelper {
+    private GradesAdapter grades_adapter;
+    private RecyclerView grades_recycler_view;
+
+    List<Grade> grades = new ArrayList<>();
+    public Double totalQPI = 0.0;
+    public Double totalUnits = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Make to run your application only in portrait mode
-
-
 
         TextView desired_qpi_tx = findViewById(R.id.desired_QPI_TXBX);
         TextView units_tx = findViewById(R.id.units_left_TXBX);
@@ -66,6 +70,34 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        FloatingActionButton act = findViewById(R.id.addRow);
+        act.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View v){
+                Grade temp = new Grade();
+                temp.numberOfunits = 3;
+                temp.letterGrade = "A";
+                grades.add(temp);
+
+                totalQPI += 12;
+                totalUnits += 3;
+
+                refreshRecyclerView();
+                refreshData();
+            }
+        });
+
+        refreshRecyclerView();
+    }
+
+    public void refreshRecyclerView(){
+        grades_recycler_view = findViewById(R.id.grades_rview);
+        grades_adapter = new GradesAdapter(this, grades, this);
+        grades_recycler_view.setAdapter(grades_adapter);
+        grades_recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        grades_recycler_view.setItemAnimator(new DefaultItemAnimator());
+        ItemTouchHelper.SimpleCallback callback = new GradesAdapterHelper(0, ItemTouchHelper.LEFT, this);
+        new ItemTouchHelper(callback).attachToRecyclerView(grades_recycler_view);
     }
 
     public void refreshCumulative(){
@@ -76,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
         try
         {
-            temp = cm.getMinRequired(Double.parseDouble(desired_qpi_tx.getText().toString()), Double.parseDouble(units_tx.getText().toString()));
+            temp = getMinRequired(Double.parseDouble(desired_qpi_tx.getText().toString()), Double.parseDouble(units_tx.getText().toString()));
         }
         catch(NumberFormatException e)
         {
@@ -92,115 +124,77 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void addRowClick (View v){
-        addRows();
-    }
-
-    public void addRows(){
-        cm.getMinRequired(2.0,2.0);
-        LinearLayout list = findViewById(R.id.main_list);
-        LinearLayout vertList = new LinearLayout(this);
-        vertList.setVerticalGravity(1);
-        vertList.setId(count);
-
-        int width1 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, getResources().getDisplayMetrics());
-        int width2 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 65, getResources().getDisplayMetrics());
-        int width3 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics());
-        int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, getResources().getDisplayMetrics());
-
-        Space sp1 = new Space(this);
-        sp1.setLayoutParams(new LinearLayout.LayoutParams(width1, height));
-        Space sp2 = new Space(this);
-        sp2.setLayoutParams(new LinearLayout.LayoutParams(width2, height));
-        Space sp3 = new Space(this);
-        sp3.setLayoutParams(new LinearLayout.LayoutParams(width3, height));
-
-        int width4 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics());
-
-        ArrayList<String> spinnerArray = new ArrayList<>();
-        spinnerArray.add("A");
-        spinnerArray.add("B+");
-        spinnerArray.add("B");
-        spinnerArray.add("C+");
-        spinnerArray.add("C");
-        spinnerArray.add("D");
-        spinnerArray.add("F/FD");
-        Spinner spinner1 = new Spinner(this);
-        spinner1.setLayoutParams(new Spinner.LayoutParams(width4, height));
-        ArrayAdapter<String> spinnerArrayAdapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, spinnerArray);
-        spinner1.setAdapter(spinnerArrayAdapter1);
-        spinner1.setId(count+1);
-
-        ArrayList<String> spinnerArray2 = new ArrayList<>();
-        spinnerArray2.add("9");
-        spinnerArray2.add("6");
-        spinnerArray2.add("5");
-        spinnerArray2.add("3");
-        spinnerArray2.add("2");
-        spinnerArray2.add("1");
-        Spinner spinner2 = new Spinner(this);
-        spinner2.setLayoutParams(new Spinner.LayoutParams(width4, height));
-        ArrayAdapter<String> spinnerArrayAdapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, spinnerArray2);
-        spinner2.setAdapter(spinnerArrayAdapter2);
-        spinner2.setId(count+2);
-        spinner2.setSelection(3);
-
-        spinner1.setOnItemSelectedListener(getOnSpinnerClick(spinner1, spinner2));
-        spinner2.setOnItemSelectedListener(getOnSpinnerClick(spinner1, spinner2));
-
-        Button btn = new Button(this);
-        btn.setOnClickListener(getOnButtonClick(count, vertList, list));
-        btn.setId(count+3);
-        btn.setBackground(getResources().getDrawable(android.R.drawable.ic_menu_close_clear_cancel));
-        btn.setMaxHeight(10);
-
-        count += 100;
-
-        vertList.addView(sp1);
-        vertList.addView(spinner1);
-        vertList.addView(sp2);
-        vertList.addView(spinner2);
-        vertList.addView(sp3);
-        vertList.addView(btn);
-        list.addView(vertList);
-
-        refreshData();
-    }
-
-    View.OnClickListener getOnButtonClick(final int count, final LinearLayout vert, final LinearLayout list)  {
-        return new View.OnClickListener() {
-            public void onClick(View v) {
-                list.removeView(vert);
-                cm.removeData(count);
-                refreshData();
-                refreshCumulative();
-            }
-        };
-    }
-
-    AdapterView.OnItemSelectedListener getOnSpinnerClick(final Spinner spin1, final Spinner spin2)  {
-        return new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                int a = spin1.getId();
-                if (spin1.getSelectedItem().toString() != "-" && spin2.getSelectedItem().toString() != "-") {
-                    cm.addData(a - 1, getEquivalent(spin1.getSelectedItem().toString()), Double.parseDouble(spin2.getSelectedItem().toString()));
-                }
-                refreshData();
-                refreshCumulative();
-            }
-
-            public void onNothingSelected(AdapterView<?> arg0) {
-
-            }
-
-        };
-    }
-
     void refreshData(){
-        cm.calculateData();
         TextView tv = findViewById(R.id.your_qpi_LBL);
-        Double ans = cm.getTotalUnits() != 0 ?  (cm.getTotalQPI()/cm.getTotalUnits()) : 0.0;
+        Double ans = totalUnits != 0 ?  (totalQPI/totalUnits) : 0.0;
         animate(tv, tv.getText().toString(), String.format("%.2f", ans));
+    }
+
+    private void animate(final TextView textview, String start, String end){
+        ValueAnimator animator = ValueAnimator.ofFloat(Float.parseFloat(start), Float.parseFloat(end));
+        animator.setDuration(300);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                textview.setText( String.format("%.2f", animation.getAnimatedValue()));
+            }
+        });
+        animator.start();
+    }
+
+    private int counter;
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+        if (viewHolder instanceof GradesAdapter.GradesViewHolder ){
+            final int deleteIndex = viewHolder.getAdapterPosition();
+            final Grade grade = grades.get(deleteIndex);
+
+            grades_adapter.removeGrade(deleteIndex);
+            totalUnits -= grade.numberOfunits;
+            totalQPI -= grade.numberOfunits * getEquivalent(grade.letterGrade);
+            counter ++;
+            refreshData();
+
+            ConstraintLayout layout = findViewById(R.id.main_layout);
+
+            Snackbar snackbar = Snackbar.make(layout, "Grade removed", Snackbar.LENGTH_LONG);
+            snackbar.setAction("UNDO", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    grades_adapter.restoreGrade(grade, deleteIndex);
+                    totalUnits += grade.numberOfunits;
+                    totalQPI += grade.numberOfunits * getEquivalent(grade.letterGrade);
+
+                    refreshData();
+                }
+            });
+            snackbar.setActionTextColor(Color.WHITE);
+            snackbar.show();
+
+            if (counter > 3){
+                Snackbar snackbarClear = Snackbar.make(layout, "Would you like to clear all grades?", Snackbar.LENGTH_LONG);
+                snackbarClear.setAction("CLEAR ALL", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        grades_adapter.clear();
+                        totalUnits = 0.0;
+                        totalQPI = 0.0;
+                        refreshData();
+                        counter = 0;
+                    }
+                });
+                snackbarClear.setActionTextColor(Color.WHITE);
+                snackbarClear.show();
+            }
+        }
+    }
+
+    public double getMinRequired(Double desired, Double unitsLeft){
+        Double tempUnits = totalUnits;
+        Double tempQPI = totalQPI;
+        Double answer = ((desired * (tempUnits + unitsLeft)) - tempQPI) / unitsLeft;
+
+        return answer;
     }
 
     Double getEquivalent(String a){
@@ -219,16 +213,5 @@ public class MainActivity extends AppCompatActivity {
                 return 1.0;
             default: return 0.0;
         }
-    }
-
-    private void animate(final TextView textview, String start, String end){
-        ValueAnimator animator = ValueAnimator.ofFloat(Float.parseFloat(start), Float.parseFloat(end));
-        animator.setDuration(300);
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            public void onAnimationUpdate(ValueAnimator animation) {
-                textview.setText( String.format("%.2f", animation.getAnimatedValue()));
-            }
-        });
-        animator.start();
     }
 }
